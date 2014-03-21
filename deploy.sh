9fff338a1daea73753f37e4d62ffe505a0f5e91a
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-21 21:54:59 +0100

    Add file moving to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cp -R 2014/* build/
 cd build
-git add -f .
-git commit -m "Deploy to gh-pages"
-git push -fq git@github.com:ThibWeb/talks.git "master:gh-pages"
+#git add -f .
+#git commit -m "Deploy to gh-pages"
+# git push -fq git@github.com:ThibWeb/talks.git "master:gh-pages"
 
