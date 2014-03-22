3796480a2aef25f907f3b9215a836a07913d7eba
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-22 12:44:53 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cp -R 2014/* build/
+rm -rf build/*/node_modules
 cp -R root/* build/
 cd build
 git add -f .
+git add -fu .
 git commit -m "Deploy to gh-pages"
 git push -fq git@github.com:ThibWeb/talks.git "master:gh-pages"
 
