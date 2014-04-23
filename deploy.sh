89a92137cd2a939cefe8d2cc5881fc7102c962af
Thibaud Colas <thibaudcolas@gmail.com>
2014-04-23 08:50:31 +0200

    Update build script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+echo "Starting build"
+
 cp -R 2014/* build/
 rm -rf build/*/node_modules
+rm -rf build/**/node_modules
 cp -R root/* build/
 cd build
+
+echo "Finished copying files"
+
 git add -f .
 git add -fu .
 git commit -m "Deploy to gh-pages"
+
+echo "Starting push"
+
 git push -fq git@github.com:ThibWeb/talks.git "master:gh-pages"
 
+echo "Build finished"
